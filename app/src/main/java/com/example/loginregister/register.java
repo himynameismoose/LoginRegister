428ea1b28f186200d3f38eeaee15856506c67474
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-30 14:31:12 -0700

    Set up login and register buttons to navigate between activities

--- app/src/main/java/com/example/loginregister/register.java
+++ app/src/main/java/com/example/loginregister/register.java
@@ -2,13 +2,27 @@ package com.example.loginregister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class register extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(register.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
